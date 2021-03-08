06b63c023e569ffd90902690f60b895cbf67ad21
HanJin Park <phj9020@hotmail.com>
2021-03-08 20:57:19 +0900

    axios baseURL changed ( firebase deploy --only functions )

--- src/components/axios.js
+++ src/components/axios.js
@@ -3,7 +3,9 @@ import axios from "axios";
 
 const instance = axios.create({
     // The API(cloud function) URL
-    baseURL: 'http://localhost:5001/clone-f8fa9/us-central1/api' 
+    baseURL: 'https://us-central1-clone-f8fa9.cloudfunctions.net/api'
+    // localhost ::  'http://localhost:5001/clone-f8fa9/us-central1/api' 
+    
 });
 
 
